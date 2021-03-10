c2cf70ba863b2732b14c42b0cadc5767e756be08
Marvin <aguirreestuardo758@gmail.com>
2021-03-09 22:33:55 -0600

    Se agrego un nuevo directorio

---
+++ Tarea-Randoms-Arrays-Metodos/PIEDRA_PAPEL_TIJERA.js
@@ -0,0 +1,109 @@
+// Your code here!
+ var J1=Math.floor(Math.random()*3);
+ var J2=Math.floor(Math.random()*3);
+ console.log("JUEGO PIEDRA, PAPEL O TIJERA");
+ console.log("-----------------------------")
+ 
+ if(J1==0&&J2==0){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("piedra")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("piedra")
+     
+     console.log("EMPATE")
+ }
+  
+
+  if(J1==1 && J2==1){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("papel")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("papel")
+     console.log("EMPATE")
+ }
+ 
+   if(J1==2&&J2==2){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("tijera")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("tijera")
+     console.log("EMPATE")
+ }
+ 
+ if(J1==0&&J2==1){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("piedra")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("papel")
+     
+     console.log("GANA JUGADOR 2")
+ }
+ if(J1==0&&J2==2){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("piedra")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("tijera")
+     
+     console.log("GANA JUGADOR 1")
+ }
+ if(J1==1&&J2==0){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("papel")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("piedra")
+     
+     console.log("GANA JUGADOR 1")
+ }
+ if(J1==1&&J2==2    ){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("papel")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("tijera")
+     
+     console.log("GANA JUGADOR 2")
+     
+ }
+ if(J1==2&&J2==0){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("tijera")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("piedra")
+     
+     console.log("GANA JUGADOR 2")
+ }
+ if(J1==2&&J2==1){
+     console.log("JUGADOR 1.")
+     console.log("Piedra, papel o tijera?")
+     console.log("tijera")
+     
+     console.log("JUGADOR 2.")
+     console.log("Piedra, papel o tijera?")
+     console.log("papel")
+     
+     console.log("GANA JUGADOR 1")
+     
+ }
